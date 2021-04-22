2bcbe547861d697d9ed694326f524896bb9adaf4
akashkaturi <64057380+akashkaturi@users.noreply.github.com>
2021-04-22 14:20:18 +0530

    colors

--- javaScript/nav.js
+++ javaScript/nav.js
@@ -2,7 +2,8 @@ const navSlide = () => {
     const burger = document.querySelector('.hamburger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
-
+    const langlist = document.querySelectorAll('#us .upperUs li');
+  
     burger.addEventListener('click', () => {
         nav.classList.toggle('open');
         //burger animation
@@ -13,11 +14,20 @@ const navSlide = () => {
                 link.style.animation = '';
             } else {
                 // k = index / 9 + 1;
-                link.style.animation = `navLinkFade 1s ease forwards ${index/9}s`;
+                link.style.animation = `navLinkFade 1s ease forwards ${index / 9}s`;
                 // console.log(index / 7);
                 // console.log(`this is ${index} value`);
             }
         });
     });
+    window.addEventListener('scroll',()=>{
+        langlist.forEach((link,index) =>{
+                // k = index / 9 + 1;
+                link.style.animation = `uslist 1s ease forwards ${(index / 2)+1}s`;
+                
+                // console.log(`this is ${index} value`);
+            
+        })
+    })
 };
 navSlide();
